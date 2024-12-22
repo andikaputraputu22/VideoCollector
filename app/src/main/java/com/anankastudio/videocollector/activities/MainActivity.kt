420daeba4648e23fd5a086451dacdab16341e5f2
I Putu Andika Putra <andikaputraputu22@gmail.com>
2024-12-23 00:09:07 +0700

    Adjust theme on application

--- app/src/main/java/com/anankastudio/videocollector/activities/MainActivity.kt
+++ app/src/main/java/com/anankastudio/videocollector/activities/MainActivity.kt
@@ -1,21 +1,44 @@
 package com.anankastudio.videocollector.activities
 
+import android.os.Build
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.WindowInsetsController
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.content.ContextCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.anankastudio.videocollector.R
+import com.anankastudio.videocollector.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupStatusBar()
+    }
+
+    private fun setupStatusBar() {
+        window?.apply {
+            statusBarColor = ContextCompat.getColor(this@MainActivity, R.color.bg_color)
+            navigationBarColor = ContextCompat.getColor(this@MainActivity, R.color.bg_color)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                decorView.windowInsetsController?.setSystemBarsAppearance(
+                    0,
+                    WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+                )
+                decorView.windowInsetsController?.setSystemBarsAppearance(
+                    0,
+                    WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS
+                )
+            } else {
+                val windowInsetsController = WindowInsetsControllerCompat(this, decorView)
+                windowInsetsController.isAppearanceLightStatusBars = false
+                windowInsetsController.isAppearanceLightNavigationBars = false
+            }
         }
     }
 }
