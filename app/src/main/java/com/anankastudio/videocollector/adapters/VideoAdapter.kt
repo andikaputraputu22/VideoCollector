18f1ed26667064d29892049368716fe36a983c73
I Putu Andika Putra <andikaputraputu22@gmail.com>
2024-12-25 12:53:06 +0700

    WIP Display popular video

---
+++ app/src/main/java/com/anankastudio/videocollector/adapters/VideoAdapter.kt
@@ -0,0 +1,32 @@
+package com.anankastudio.videocollector.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.anankastudio.videocollector.databinding.ItemVideoBinding
+import com.anankastudio.videocollector.models.Video
+import com.anankastudio.videocollector.viewholders.ItemVideo
+
+class VideoAdapter : RecyclerView.Adapter<ItemVideo>() {
+
+    private var listVideo: MutableList<Video> = mutableListOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemVideo {
+        val binding = ItemVideoBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        )
+        return ItemVideo(binding)
+    }
+
+    override fun getItemCount() = listVideo.size
+
+    override fun onBindViewHolder(holder: ItemVideo, position: Int) {
+        holder.bind()
+    }
+
+    fun setData(items: List<Video>) {
+        listVideo.clear()
+        listVideo.addAll(items)
+        notifyDataSetChanged()
+    }
+}
---
+++ app/src/main/java/com/anankastudio/videocollector/viewholders/ItemVideo.kt
@@ -0,0 +1,13 @@
+package com.anankastudio.videocollector.viewholders
+
+import androidx.recyclerview.widget.RecyclerView
+import com.anankastudio.videocollector.databinding.ItemVideoBinding
+
+class ItemVideo(
+    private val binding: ItemVideoBinding
+) : RecyclerView.ViewHolder(binding.root) {
+
+    fun bind() {
+
+    }
+}
